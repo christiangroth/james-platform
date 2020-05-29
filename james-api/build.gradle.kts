e5496dbaeca59b35dcd694c999cd245507fc989f
Christian Groth <2071856+christiangroth@users.noreply.github.com>
2020-05-29 16:25:38 +0200

    convert gradle scripts to kotlin

---
+++ james-api/build.gradle.kts
@@ -0,0 +1,70 @@
+
+// TODO unable to compile when offline ... why??
+plugins {
+    id("application")
+//    id("com.palantir.docker-run") version "0.25.0"
+
+    kotlin("jvm") version "1.3.72"
+    id("de.chrgroth.gradle.restcrud") version "0.1.0-SNAPSHOT"
+}
+
+application {
+    group = "de.chrgroth"
+    mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    jcenter()
+    // maven { url "https://kotlin.bintray.com/ktor" }
+}
+
+val versionKtor = "1.3.0"
+val versionLogback = "1.2.1"
+val versionSemVer = "1.1.1"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$versionKtor")
+    implementation("io.ktor:ktor-server-netty:$versionKtor")
+    implementation("io.ktor:ktor-server-core:$versionKtor")
+    implementation("io.ktor:ktor-locations:$versionKtor")
+    implementation("io.ktor:ktor-metrics:$versionKtor")
+    implementation("io.ktor:ktor-server-host-common:$versionKtor")
+    implementation("io.ktor:ktor-jackson:$versionKtor")
+
+    implementation("ch.qos.logback:logback-classic:$versionLogback")
+    implementation("net.swiftzer.semver:semver:$versionSemVer")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.+")
+    implementation("org.litote.kmongo:kmongo:3.12.+")
+
+    testImplementation("io.ktor:ktor-server-tests:$versionKtor")
+}
+
+/*dockerRun {
+    name 'james-api-test-mongodb'
+    image 'mongo:4.2.3'
+    // volumes 'hostvolume': '/containervolume'
+    ports '27017:27017'
+    daemonize true
+    clean true
+    env 'MONGO_INITDB_ROOT_USERNAME': 'james', 'MONGO_INITDB_ROOT_PASSWORD': 'semaj'
+    // command 'sleep', '100'
+    // arguments '--hostname=custom', '-P'
+
+    // MONGO_INITDB_DATABASE
+    // This variable allows you to specify the name of a database to be used for creation scripts in
+    // /docker-entrypoint-initdb.d/*.js (see Initializing a fresh instance below). MongoDB is fundamentally designed for
+    // "create on first use", so if you do not insert data with your JavaScript files, then no database is created.
+}
+
+task waitAfterDockerStart() {
+    doLast {
+        sleep 5000
+    }
+}
+
+project.tasks.findByPath("dockerRun").finalizedBy("waitAfterDockerStart")
+test.dependsOn("dockerRun")
+test.finalizedBy("dockerStop")
+*/
