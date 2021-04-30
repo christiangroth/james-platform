0f58b4736693de889d6b1805e77ec06f73912d3c
Christian Groth <2071856+christiangroth@users.noreply.github.com>
2021-04-30 23:46:34 +0200

    fixes issue #2 using latest dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     kotlin("jvm") version "1.4.20"
     id("net.researchgate.release") version "2.8.1"
 
-    id("se.patrikerdes.use-latest-versions") version "0.2.14"
-    id("com.github.ben-manes.versions") version "0.33.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.16"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 repositories {
