e12f6d3641d8fd402cb49eac4a83f6d8476fe83c
Christian Groth <2071856+christiangroth@users.noreply.github.com>
2021-08-31 20:39:40 +0200

    fixes issue #3 cleaned up query and persistence interfaces

--- core/src/main/kotlin/de/chrgroth/james/data/Persistence.kt
+++ core/src/main/kotlin/de/chrgroth/james/data/Persistence.kt
@@ -6,25 +6,27 @@ import de.chrgroth.james.user.User
 import de.chrgroth.james.user.UserWorkspace
 import java.util.UUID
 
-// TODO #3 split to user and workspace persistence
-
 interface UserQueryPersistencePort {
     fun get(id: UUID): Maybe<User?>
     fun getByEmail(email: String): Maybe<User?>
 
     // TODO #16 what about paging and how to design filter parameters??
     fun find(): Maybe<Set<User>>
-
-    fun getWorkspaces(userId: UUID): Maybe<Set<UserWorkspace>>
-    fun getWorkspace(userId: UUID, workspaceId: UUID): Maybe<UserWorkspace?>
-
-    // TODO #16 what about paging and how to design filter parameters??
-    fun findWorkspaces(userId: UUID): Maybe<Set<UserWorkspace>>
 }
 
-// TODO #3 split to user and workspace persistence
+interface UserWorkspaceQueryPersistencePort {
+    fun get(userId: UUID, workspaceId: UUID): Maybe<UserWorkspace?>
+
+    // TODO #16 what about paging and how to design filter parameters??
+    fun find(userId: UUID): Maybe<Set<UserWorkspace>>
+}
 
 interface UserCommandPersistencePort {
     fun upsert(item: User): Maybe<User>
     fun delete(id: UUID): Maybe<Unit>
+}
+
+interface UserWorkspaceCommandPersistencePort {
+    fun upsert(userId: UUID, item: User): Maybe<User>
+    fun delete(userId: UUID, id: UUID): Maybe<Unit>
 }
--- core/src/main/kotlin/de/chrgroth/james/data/Query.kt
+++ core/src/main/kotlin/de/chrgroth/james/data/Query.kt
@@ -10,7 +10,9 @@ interface UserQueryPort {
     fun getUser(id: UUID): Maybe<User?>
     fun getUserByEmail(email: String): Maybe<User?>
     fun findUsersByNameInfix(nameInfix: String): Maybe<Set<User>>
+}
 
+interface UserWorkspaceQueryPort {
     fun getWorkspaces(userId: UUID): Maybe<Set<UserWorkspace>>
     fun getWorkspace(userId: UUID, workspaceId: UUID): Maybe<UserWorkspace?>
     fun findWorkspacesByNameInfix(userId: UUID, nameInfix: String): Maybe<Set<UserWorkspace>>
@@ -35,20 +37,22 @@ internal class UserQueryAdapter(private val queryPersistence: UserQueryPersisten
             result.filter { it.name.matches(Regex(".*${nameInfix}.*")) }.toSet()
         }
     }
+}
+
+internal class UserWorkspaceQueryAdapter(private val queryPersistence: UserWorkspaceQueryPersistencePort) : UserWorkspaceQueryPort {
 
     override fun getWorkspaces(userId: UUID): Maybe<Set<UserWorkspace>> {
-        return queryPersistence.getWorkspaces(userId)
+        return queryPersistence.find(userId)
     }
 
     override fun getWorkspace(userId: UUID, workspaceId: UUID): Maybe<UserWorkspace?> {
-        return queryPersistence.getWorkspace(userId, workspaceId)
+        return queryPersistence.get(userId, workspaceId)
     }
 
     // TODO #16 move filtering to persistence
     override fun findWorkspacesByNameInfix(userId: UUID, nameInfix: String): Maybe<Set<UserWorkspace>> {
-        return queryPersistence.findWorkspaces(userId).map { result ->
+        return queryPersistence.find(userId).map { result ->
             result.filter { it.name.matches(Regex(".*${nameInfix}.*")) }.toSet()
         }
     }
 }
-
