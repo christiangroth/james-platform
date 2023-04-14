5dfb8346114ed901393a2483d32cec888d940f3c
christiangroth <2071856+christiangroth@users.noreply.github.com>
2023-04-14 13:31:05 +0200

    fixes #7 restructured release notes model and handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,9 @@ plugins {
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.46.0"
 }
+
+// TODO #ticket?!?!
+// call task koverMergedReport for merged report manually
+koverMerged {
+    enable()
+}
