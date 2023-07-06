de467b24c579cc1d0f67334c81e58c5641d0ca56
Chris <chris@Cliff>
2023-07-06 21:42:51 +0200

    gradle wrapper upgrade, added ktor runtime module, added some static bootstrap templates to play around with

---
+++ runtime-ktor/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("kotlin-project")
+    id("io.ktor.plugin") version "2.3.2"
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-netty")
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
